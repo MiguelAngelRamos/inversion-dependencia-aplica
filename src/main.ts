5e4fa71c8298cd570ea8b78ea3369838454375fe
Miguel Angel Ramos <return.infox@gmail.com>
2023-12-07 17:25:59 -0300

    implementar mensaje desde facebook

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { EmailNotifier } from "./class/EmailNotifier";
+import { FacebookNotifier } from "./class/FacebookNotifier";
 import { WhatsAppNotifier } from "./class/WhatsAppNotifier";
 import { INotifier } from "./interface/INotifier";
 import { NotifierService } from "./service/NotifierService";
 
 const notifiers: INotifier [] = [
   new EmailNotifier(),
-  new WhatsAppNotifier()
+  new WhatsAppNotifier(),
+  new FacebookNotifier()
 ];
 
 const service = new NotifierService(notifiers);
